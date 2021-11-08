730dfa42ff977a3e942bdc93442cf6a40b3de009
fikerte <tofikertesfaye@gmail.com>
2021-11-09 00:29:32 +0300

    Structure application files

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1 className="logo">BookStore</h1>
+            <ul>
+                <li>
+                    <Link to="/">Books</Link>
+                </li>
+                <li>
+                    <Link to="/categories">Categories</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/components/books.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Books = () => {
+    <h1>This is books page</h1>
+}
+export default Books;
