7abde67702f52a282329731b81813eac2c83048e
fikerte <tofikertesfaye@gmail.com>
2021-11-09 11:40:54 +0300

    Add linters config files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Books from "./components/books"
+import Books from './components/books';
 import Categories from './components/categories';
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <h1 className="logo">BookStore</h1>
-            <ul>
-                <li>
-                    <Link to="/">Books</Link>
-                </li>
-                <li>
-                    <Link to="/categories">Categories</Link>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+const Navbar = () => (
+  <nav className="navbar">
+    <h1 className="logo">BookStore</h1>
+    <ul>
+      <li>
+        <Link to="/">Books</Link>
+      </li>
+      <li>
+        <Link to="/categories">Categories</Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
