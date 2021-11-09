7d71749588b7437667d6997076a9799faf58941d
fikerte <tofikertesfaye@gmail.com>
2021-11-09 17:48:35 +0300

    Add dispatcher to 'books' component to add new book

--- src/components/books.js
+++ src/components/books.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const Books = () => (
-  <div>
-    <h3>ADD NEW BOOK</h3>
-    <input type="text" placeholder="Book title" />
-    <select name="Category">
-      <option value="">Category</option>
-    </select>
-    <button type="submit">ADD BOOK</button>
-  </div>
+const Books = () => {
+   const dispatch = useDispatch()
+   const submitBookToStore = () => {
+      const newBook = {
+         id: 1,
+         title: "Atse Minilik",
+         author: "Paulos Ngongo"
+      }
+      dispatch(addBook(newBook))
+   }
 
-);
+  return (
+   <div>
+   <h3>ADD NEW BOOK</h3>
+   <input type="text" placeholder="Book title" />
+   <select name="Category">
+     <option value="">Category</option>
+   </select>
+   <button type="submit" onClick={submitBookToStore}>ADD BOOK</button>
+ </div>
+  )
+
+  };
 export default Books;
