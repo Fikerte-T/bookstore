b1c2e5e77da3bf889cc68f8de0c29ce8f147cdf9
fikerte <tofikertesfaye@gmail.com>
2021-11-09 11:24:25 +0300

    Add form for adding a book in books.js

--- src/components/books.js
+++ src/components/books.js
@@ -2,7 +2,15 @@ import React from "react";
 
 const Books = () => {
    return(
-    <h1>This is books page</h1>
+      <div>
+         <h3>ADD NEW BOOK</h3> 
+         <input type="text" placeholder="Book title" />
+         <select name="Category">
+            <option value="">Category</option>
+         </select>
+         <button type="submit">ADD BOOK</button>
+      </div>
+    
    )
 }
 export default Books;
