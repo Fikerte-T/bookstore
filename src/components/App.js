3f3c549931ff4784715565681e3ffb806f25de4f
fikerte <tofikertesfaye@gmail.com>
2021-11-11 21:37:12 +0300

    Fix linters

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Books from './components/Books';
-import Categories from './components/Categories';
-import Navbar from './components/Navbar';
-import store from './redux/configureStore';
+import Books from './Books';
+import Categories from './Categories';
+import Navbar from './Navbar';
+import store from '../redux/configureStore';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
